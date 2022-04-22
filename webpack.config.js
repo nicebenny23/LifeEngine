ce00767626af3318f92e9a26bcd8aab2c51c8633
nicebenny23 <bcharnow09@gmail.com>
2022-04-22 15:14:59 +0000

    monke

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,12 @@
 const path = require('path');
-const webpack = require("webpack");
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist/js/'),
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-        jquery: 'jQuery'    
-        })
-    ]
-};
+ module.exports = {
+     entry: './src/index.js',
+     output: {
+         filename: 'bundle.js',
+         path: path.resolve(__dirname, 'dist/js/'),
+     },
+     externals: {
+         jquery: 'jQuery'
+     }
+    }
